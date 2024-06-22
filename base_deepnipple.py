c0946e357ae585b11b7928b9b9fa7263d224f925
gnkgo <joanna.brodbeck@hotmail.ch>
2024-06-22 20:56:58 +0200

    new folder

--- base_deepnipple.py
+++ base_deepnipple.py
@@ -6,25 +6,33 @@ alfonsomedela.com
 '''
 
 import argparse
-
+import os
 from utils.code.deep_nipple import DeepNipple
 
-parser = argparse.ArgumentParser(description='DeepNipple algorithm')
-parser.add_argument('--img_path', type=str, help='path to the input image')
-parser.add_argument('--mode', type=str, default='seg', help='seg or bbox mode')
-parser.add_argument('--show', type=bool, default=True, help='show the output')
+def process_images_in_folder(folder_path, alg_mode, show, save, save_path):
+    # Get list of all image files in the folder
+    image_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    
+    for image_file in image_files:
+        image_path = os.path.join(folder_path, image_file)
+        print(f'Processing {image_path}...')
+        output = DeepNipple(image_path, alg_mode, show, save, save_path)
+        print(f'Output for {image_path}: {output}')
 
+parser = argparse.ArgumentParser(description='DeepNipple algorithm')
+parser.add_argument('--img_folder', type=str, default = '/home/cluster/jbrodbec/DeepNipple/sexy', help='path to the folder containing input images')
+parser.add_argument('--mode', type=str, default='bbox', help='seg or bbox mode')
+parser.add_argument('--show', type=bool, default=False, help='show the output')
+parser.add_argument('--save', type=bool, default=True, help='save the output')
+parser.add_argument('--save_path', type=str, default='segmentation_output', help='path to save the output')
 
 if __name__ == '__main__':
-
     print('Running DeepNipple...')
-
     args = parser.parse_args()
-    image_path = args.img_path
+    folder_path = args.img_folder
     alg_mode = args.mode
     show = args.show
+    save = args.save
+    save_path = args.save_path
 
-    output = DeepNipple(image_path, alg_mode, show)
-
-
-
+    process_images_in_folder(folder_path, alg_mode, show, save, save_path)
--- utils/code/deep_nipple.py
+++ utils/code/deep_nipple.py
@@ -1,40 +1,57 @@
 from fastai.vision import *
 import cv2
-
+import matplotlib.pyplot as plt
 from utils.code.aux_func import seg2bbox, predict
+import os
+import uuid
+from datetime import datetime
 
-def DeepNipple(image_path, alg_mode, show=True):
+def DeepNipple(image_path, alg_mode, show, save, save_path):
     '''
     :param image_path: input image absolute path
     :param alg_mode: seg/bbox
-    :param device: cpu or gpu number
+    :param show: boolean, whether to show the image or not
+    :param save: boolean, whether to save the mask or not
+    :param save_path: folder to save the mask/bounding boxes
     :return: segmentation mask / bounding boxes
     '''
 
-    # load pyrtoch model
+    # Load pytorch model
     learner_path = 'utils/models/base-model/'
     learner = load_learner(learner_path)
 
     image, mask = predict(image_path, learner)
     print(image.shape, mask.shape)
 
+    # Ensure the save_path is a directory
+    if save and not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    # Extract the base name of the image file
+    image_name = os.path.basename(image_path)
+    image_base_name = os.path.splitext(image_name)[0]  # Get the file name without extension
+
     if alg_mode == 'seg':
-
         output = mask
+        # if show:
+        #     plt.subplot(121)
+        #     plt.imshow(image)
+        #     plt.title('Original image')
+        #     plt.axis('off')
+        #     plt.subplot(122)
+        #     plt.imshow(image)
+        #     plt.axis('off')
+        #     plt.imshow(mask[:, :, 1], alpha=0.6, interpolation='bilinear', cmap='magma')
+        #     plt.axis('off')
+        #     plt.imshow(mask[:, :, 2], alpha=0.6, interpolation='bilinear', cmap='afmhot')
+        #     plt.axis('off')
+        #     plt.title('NAC segmentation')
+        #     plt.show()
 
-        plt.subplot(121)
-        plt.imshow(image)
-        plt.title('Original image')
-        plt.axis('off')
-        plt.subplot(122)
-        plt.imshow(image)
-        plt.axis('off')
-        plt.imshow(mask[:, :, 1], alpha=0.6, interpolation='bilinear', cmap='magma')
-        plt.axis('off')
-        plt.imshow(mask[:, :, 2], alpha=0.6, interpolation='bilinear', cmap='afmhot')
-        plt.axis('off')
-        plt.title('NAC segmentation')
-        plt.show()
+        if save:
+            mask_save_path = os.path.join(save_path, f'{image_base_name}.png')
+            mask_to_save = (mask * 255).astype(np.uint8)
+            cv2.imwrite(mask_save_path, mask_to_save)
 
     else:
         coords = seg2bbox(mask)
@@ -45,7 +62,30 @@ def DeepNipple(image_path, alg_mode, show=True):
                 y1, y2, x1, x2 = coor[0], coor[1], coor[2], coor[3]
                 cv2.rectangle(image, (x1, y1), (x2, y2), (36, 255, 12), 2, -1)
 
-            plt.imshow(image)
+            # Convert image to RGB before showing with matplotlib
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            plt.imshow(image_rgb)
             plt.show()
+
+        if save:
+            for idx, coor in enumerate(coords):
+                y1, y2, x1, x2 = coor[0], coor[1], coor[2], coor[3]
+
+                # Ensure coordinates are within image boundaries
+                if x1 >= image.shape[1] or x2 >= image.shape[1] or y1 >= image.shape[0] or y2 >= image.shape[0]:
+                    print(f"Warning: Invalid bbox coordinates for {image_base_name}_bbox_{idx}.png")
+                    continue  # Skip saving this bbox image
+
+                bbox_image = image[y1:y2, x1:x2]
+
+                # Check if bbox_image is empty or None
+                if bbox_image.size == 0:
+                    print(f"Warning: Empty bbox image for {image_base_name}_bbox_{idx}.png")
+                    continue  # Skip saving this bbox image
+
+                # Convert bbox_image to BGR before saving with OpenCV
+                bbox_image_bgr = cv2.cvtColor(bbox_image, cv2.COLOR_RGB2BGR)
+                bbox_save_path = os.path.join(save_path, f'{image_base_name}_bbox_{idx}.png')
+                cv2.imwrite(bbox_save_path, bbox_image_bgr)
 
     return output
